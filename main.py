5717b1f7dd815c5bd0e870400cc99928b9134f12
Dario Ljubas <darioljubas33@gmail.com>
2023-12-12 20:04:42 +0100

    #1 Made a function that walks through the whole folder
    Currently, the function just counts the files inside.

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import matplotlib as mpl
+
+import os
+
+def fn(dir):
+    num_of_files = 0
+    for root, subdirs, files in os.walk(dir):
+        if len(files) != 0:
+            num_of_files += len(files)
+
+    return num_of_files
+
+fn("lfw")
